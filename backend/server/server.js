b3005bfef3a19b8f379d0056f07a7faa9440c2a1
bosssanova <k.kamalanantawong@hotmail.com>
2020-03-18 16:25:46 +0700

    adding message

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const passport = require("passport")
 const app = express()
+const path = require('path')
 let cors = require('cors')
 
 const users = require("../routes/user.route")
@@ -23,7 +24,7 @@ app.use(bodyParser.json())
 // DB Config
 const db = require("../config/keys").mongoURI
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err))
 
@@ -43,4 +44,11 @@ app.use('/products', product)
 app.use('/productlog', productlog)
 app.use("/image", image)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'clientm', 'build', 'index.html'))
+  })
+}
 app.listen(port, () => console.log(`Server up and running on port ${port} !`))
